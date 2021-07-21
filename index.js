6d805b6eb9a0474d781a46311bf1c9c116b173a1
JuanP Pérez <yohanam.paulus@gmail.com>
2021-07-21 15:43:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const axios = require("axios");
+
+const tmi = require("tmi.js");
+
+const opts = {
+  identity: {
+    username: process.env.CLIENT_ID,
+    password: process.env.TWITCH_BOT_TOKEN,
+  },
+  channels: [process.env.CHANNEL_ID],
+};
+const client = new tmi.client(opts);
+
+client.on("connected", onConnectedHandler);
+client.on("message", onMessageHandler);
+client.on("whisper", (from, userstate, message, self) => {
+  let message_out = response(message);
+  message_out.then((response) => {
+    client
+      .whisper(from, response.data || "No entiendo que dices")
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+});
+
+client.connect();
+
+const response = async (message) => {
+  const options = {
+    method: "GET",
+    url: process.env.NLP_URL,
+    json: true,
+    data: { frase: message },
+  };
+  return await axios(options);
+};
+
+function onMessageHandler(target, context, msg, self) {
+  if (self) return;
+
+  const commandName = msg.trim();
+
+  if (commandName.startsWith("!dice")) {
+    const sides = parseInt(commandName.split(" ")[1]);
+    const num = rollDice(sides);
+    client.say(target, `${context.username} ha sacado un ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else if (msg.toLowerCase().includes("@perjubot")) {
+    let message_out = response(msg);
+    message_out.then((response) => {
+      client
+        .say(target, response.data || "No entiendo que dices")
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+function rollDice(nSides) {
+  const sides = typeof nSides === "number" ? nSides : 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+function onConnectedHandler(addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
